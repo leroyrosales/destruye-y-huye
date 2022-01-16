2b547841c71ebe85fef1e3ca5b218ff0f16f00bc
Leroy Rosales <leroyrosales@gmail.com>
2022-01-16 14:08:30 -0600

    Adds hover state

--- components/post-preview.js
+++ components/post-preview.js
@@ -11,6 +11,8 @@ export default function PostPreview({
   return (
     <div className="relative">
       <div class="projects--content-overlay"></div>
+      <Link as={`/locations/${slug}`} href="/locations/[slug]">
+      <a className="doc_link">
       <div className="">
         <CoverImage
           slug={slug}
@@ -20,14 +22,16 @@ export default function PostPreview({
           width={600}
         />
       </div>
-      <div class="projects--content-details absolute top-0 transition-opacity opacity-0 hover:opacity-100">
+      <div class="projects--content-details absolute top-0 bottom-0 transition-opacity opacity-0 hover:opacity-100 span">
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/locations/${slug}`} href="/locations/[slug]">
-          <a className="hover:underline">{title}</a>
-        </Link>
+
+          {title}
+
       </h3>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
       </div>
+      </a>
+      </Link>
     </div>
   )
 }
