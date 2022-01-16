77648aa6a451d65368c0b40c5ace46ae972cb178
leroyrosales <leroyrosales@gmail.com>
2022-01-16 08:18:26 -0600

    Updates navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 
+const classes = "ml-8 text-lg font-bold hover:text-ut-burntorange hover:underline focus:text-ut-burntorange focus:underline"
+
 export default function Navigation() {
     return (
         <>
             <ul className="flex">
-                <li className="mr-8 text-lg font-bold hover:text-ut-burntorange hover:underline focus:text-ut-burntorange focus:underline"><Link href="/"><a>Home</a></Link></li>
-                <li className="text-lg font-bold hover:text-ut-burntorange hover:underline focus:text-ut-burntorange focus:underline"><Link href="/about"><a>About</a></Link></li>
+                <li><Link href="/"><a className={classes}>Home</a></Link></li>
+                <li><Link href="/about"><a className={classes}>About</a></Link></li>
             </ul>
         </>
     )
