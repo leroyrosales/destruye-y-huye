d317b116b2bdb7c6c586eb2fbfcf30d80bfd8917
Leroy Rosales <leroyrosales@gmail.com>
2022-01-17 10:17:30 -0600

    Adds mapbox

---
+++ components/MapBox.component.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import ReactMapboxGl, { Popup, Marker, Layer, Feature } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+require("mapbox-gl/dist/mapbox-gl.css");
+
+const Map = ReactMapboxGl({
+  accessToken: process.env.NEXT_PUBLIC_MAPBOX_API,
+  logoPosition: "top-left",
+});
+
+export default function MapBox({ coordinates }) {
+  return (
+    <div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v8"
+        containerStyle={{
+          height: "400px",
+          width: "100%",
+        }}
+        center={coordinates}
+        zoom={[13]}      >
+        <Marker
+          coordinates={coordinates}
+        >
+          <span className="text-6xl" role="img" aria-label="push-pin">
+            📌
+          </span>
+        </Marker>
+      </Map>
+    </div>
+  );
+}
--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,25 @@
 import markdownStyles from "./markdown-styles.module.css";
+import MapBox from "./MapBox.component";
 
-export default function PostBody({ content }) {
+export default function PostBody({ coords, content }) {
+  const [lat, lng] = coords.split(",");
+
+  const lngLat = {
+    "lng": 1,
+    "lat": 1
+  };
+
+  const location = Object.create(lngLat);
+
+  location.lng = lng
+  location.lat = lat
+
   return (
     <>
       <section className="grid grid-cols-1 md:grid-cols-2">
-        <div></div>
+        <div>
+           <MapBox coordinates={location} />
+        </div>
         <div className="mb-8 md:mb-16 sm:mx-0">
           <div
             className={markdownStyles["markdown"]}
