ab33a6836e4835d81a5f6d0ca987e44360c8ec6d
leroyrosales <leroyrosales@gmail.com>
2022-01-23 21:19:06 -0600

    Adds tailwind styling to form

--- components/contact-form.component.js
+++ components/contact-form.component.js
@@ -2,23 +2,23 @@ import React from "react";
 
 export default function ContactForm() {
   return (
-    <form name="contact" action="/success" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
+    <form name="survey" action="/success" method="POST" data-netlify="true">
+      <input type="hidden" value="survey" />
       <div>
-        <input type="text" name="firstname" id="firstname" />
-        <label htmlFor="yourname">Your Name:</label> <br />
-        <input type="text" name="name" id="yourname" />
+        <label htmlFor="name">Your Name:</label> <br />
+        <input className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:bg-white focus:border-gray-500"  type="text" name="firstname" id="firstname" placeholder="Jane" />
+        <input className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:bg-white focus:border-gray-500"  type="text" name="name" id="name" placeholder="Doe" />
       </div>
       <div>
-        <label htmlFor="youremail">Your Email:</label> <br />
-        <input type="email" name="email" id="youremail" />
+        <label htmlFor="email">Your Email:</label> <br />
+        <input className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:bg-white focus:border-gray-500"  type="email" name="email" id="email" placeholder="jane.doe@email.com"/>
       </div>
       <div>
-        <label htmlFor="yourmessage">Message:</label> <br />
-        <textarea name="message" id="yourmessage"></textarea>
+        <label htmlFor="message">Message:</label> <br />
+        <textarea className=" no-resize appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500 h-48 resize-none" name="message" id="message"></textarea>
       </div>
       <div>
-        <button type="submit">Send</button>
+        <button className="shadow bg-green hover:bg-blue focus:shadow-outline text-white font-bold py-2 px-4 rounded"  type="submit">Send</button>
       </div>
     </form>
   );
--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,25 @@
-export default function Success() {
-  return <div>Form successfully submitted!</div>
+import Container from "../components/container";
+import Layout from "../components/layout";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import PostTitle from "../components/post-title";
+import ContactForm from "../components/contact-form.component";
+
+export default function about() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Succcess | {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <section className="grid grid-cols-1 xl:grid-cols-2">
+            <PostTitle title="Thank you for your submission!" />
+            <div>
+            </div>
+          </section>
+        </Container>
+      </Layout>
+    </>
+  );
 }
