cbe8e1445b2c7896be162de006ad5d682b46cb8f
leroyrosales <leroyrosales@gmail.com>
2022-01-23 21:01:59 -0600

    Updates contact form to sample

--- components/contact-form.component.js
+++ components/contact-form.component.js
@@ -3,43 +3,22 @@ import React from "react";
 export default function ContactForm() {
   return (
     <form name="contact" action="/success" method="POST" data-netlify="true">
-      <input type="hidden" name="survey" value="contact" />
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-first-name">
-            First Name
-          </label>
-          <input className="appearance-none block w-full bg-gray-200 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-        </div>
-        <div className="w-full md:w-1/2 px-3">
-          <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-last-name">
-            Last Name
-          </label>
-          <input className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
-        </div>
+      <input type="hidden" name="form-name" value="contact" />
+      <div>
+        <input type="text" name="firstname" id="firstname" />
+        <label htmlFor="yourname">Your Name:</label> <br />
+        <input type="text" name="name" id="yourname" />
       </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-password">
-            E-mail
-          </label>
-          <input className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="jane.doe@email.com"/>
-        </div>
+      <div>
+        <label htmlFor="youremail">Your Email:</label> <br />
+        <input type="email" name="email" id="youremail" />
       </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-password">
-            Message
-          </label>
-          <textarea className=" no-resize appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
-        </div>
+      <div>
+        <label htmlFor="yourmessage">Message:</label> <br />
+        <textarea name="message" id="yourmessage"></textarea>
       </div>
-      <div className="md:flex md:items-center">
-        <div className="md:w-1/3">
-          <button className="shadow bg-green hover:bg-blue focus:shadow-outline text-white font-bold py-2 px-4 rounded" type="submit">
-            Send
-          </button>
-        </div>
+      <div>
+        <button type="submit">Send</button>
       </div>
     </form>
   );
