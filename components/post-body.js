221c0a88977225038f8701c30937a827d9dde756
Leroy Rosales <leroyrosales@gmail.com>
2022-02-02 22:07:23 -0600

    Adds modals

--- components/post-body.js
+++ components/post-body.js
@@ -1,5 +1,7 @@
 import Container from "./container";
 import CoverImage from "./cover-image.component";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import ModalImage from "react-modal-image";
 
 export default function PostBody({ title, coverImage, gallery, content }) {
   return (
@@ -14,7 +16,14 @@ export default function PostBody({ title, coverImage, gallery, content }) {
               width={900}
             />
             {gallery?.map((img, i) => (
-              <CoverImage key={i} src={img} />
+              <ModalImage
+                small={img}
+                large={img}
+                alt="Hello World!"
+                key={i}
+                hideDownload={true}
+                showRotate={true}
+              />
             ))}
           </div>
           <article className="mb-8 md:mb-16 sm:mx-0">
