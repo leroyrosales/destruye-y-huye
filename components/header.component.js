879da3e6cd6718b44a35436e045cf06316e4209e
Leroy Rosales <leroyrosales@gmail.com>
2022-02-05 10:29:02 -0600

    Fixed z-inex

--- components/header.component.js
+++ components/header.component.js
@@ -18,8 +18,8 @@ export default function Header() {
   };
 
   return (
-    <section className="bg-gradient-to-rpy-8 mb-16 py-8">
-        <div className={`flex flex-col px-5 md:px-10 z-25 xl:flex-row justify-between xl:items-center ${show ? "w-full fixed-nav bg-white" : ""}`}>
+    <section className="bg-gradient-to-r py-8 mb-16 py-8">
+        <div className={`flex flex-col px-5 md:px-10 z-30 xl:flex-row justify-between xl:items-center ${show ? "w-full fixed-nav bg-white" : ""}`}>
           <h1 className="text-transparent bg-clip-text bg-gradient-to-br from-green via-green to-white text-6xl mb-4 md:text-7xl font-extrabold tracking-tighter leading-tight md:pr-8">
             <Link href="/">
               <a className="hover:underline">{ CMS_NAME }</a>
