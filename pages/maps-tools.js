45241255063df76602b7d2be7750f5c4a9b4620f
Leroy Rosales <leroyrosales@gmail.com>
2022-02-05 22:16:29 -0600

    Cleans up content on pages

--- pages/maps-tools.js
+++ pages/maps-tools.js
@@ -15,17 +15,69 @@ export default function MapsAndTools() {
           <section className="grid grid-cols-1 xl:grid-cols-2">
             <PostTitle title="Maps, software &amp; online tools" />
             <div>
-              City of Austin Property Profile web map
-              https://www.austintexas.gov/GIS/PropertyProfile/ Moon Tower
-              Locations map
-              https://www.google.com/maps/d/viewer?mid=1ggPjV58m4g1NAKatjuqvERHVMW0&usp=sharing
-              Historical, and primary source materials from or about Texas.
-              https://texashistory.unt.edu/ USGS Historical Topographic Map
-              Explorer https://livingatlas.arcgis.com/topoexplorer/index.html
-              Search primary source materials held in archives, libraries,
-              museums and historical societies
-              https://researchworks.oclc.org/archivegrid/ Indigenous Peoples
-              Territories Map https://native-land.ca/
+              <ul>
+                <li className="mb-8">
+                  <h3 className="text-black text-xl mb-0">
+                    City of Austin Property Profile web map
+                  </h3>
+                  <a
+                    href="https://www.austintexas.gov/GIS/PropertyProfile/"
+                    target="_blank"
+                  >
+                    https://www.austintexas.gov/GIS/PropertyProfile/
+                  </a>
+                </li>
+                <li className="mb-8">
+                  <h3 className="text-black text-xl mb-0">Moon Tower Locations map</h3>
+                  <a
+                    href="https://www.google.com/maps/d/viewer?mid=1ggPjV58m4g1NAKatjuqvERHVMW0&usp=sharing"
+                    target="_blank"
+                  >
+                    {" "}
+                    https://www.google.com/maps/d/viewer?mid=1ggPjV58m4g1NAKatjuqvERHVMW0&usp=sharing
+                  </a>
+                </li>
+                <li className="mb-8">
+                  <h3 className="text-black text-xl mb-0">
+                    Historical, and primary source materials from or about
+                    Texas.
+                  </h3>
+                  <a href="https://texashistory.unt.edu/" target="_blank">
+                    https://texashistory.unt.edu/
+                  </a>
+                </li>
+                <li className="mb-8">
+                  <h3 className="text-black text-xl mb-0">
+                    USGS Historical Topographic Map Explorer
+                  </h3>
+                  <a
+                    href="https://livingatlas.arcgis.com/topoexplorer/index.html"
+                    target="_blank"
+                  >
+                    https://livingatlas.arcgis.com/topoexplorer/index.html
+                  </a>
+                </li>
+                <li className="mb-8">
+                  <h3 className="text-black text-xl mb-0">
+                    Search primary source materials held in archives, libraries,
+                    museums and historical societies
+                  </h3>
+                  <a
+                    href="https://researchworks.oclc.org/archivegrid/"
+                    target="_blank"
+                  >
+                    https://researchworks.oclc.org/archivegrid/
+                  </a>
+                </li>
+                <li className="mb-8">
+                  <h3 className="text-black text-xl mb-0">
+                    Indigenous Peoples Territories Map
+                  </h3>
+                  <a href="https://native-land.ca/" target="_blank">
+                    https://native-land.ca/
+                  </a>
+                </li>
+              </ul>
             </div>
           </section>
         </Container>
