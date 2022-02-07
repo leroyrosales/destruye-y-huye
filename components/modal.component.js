f7323ebc3070c2796f1128187aa4931719864ede
leroyrosales <leroyrosales@gmail.com>
2022-02-06 20:30:09 -0600

    Make modals accessible

--- components/modal.component.js
+++ components/modal.component.js
@@ -1,73 +1,159 @@
 import React from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import ReactModal from "react-modal";
 
-export default function Modal({ locationTitle, clickedImg, setClickedImg }) {
-
+export default function Modal({
+  modalIsOpen,
+  locationTitle,
+  clickedImg,
+  setClickedImg,
+  closeModal,
+}) {
   const { caption, title } = clickedImg;
-
-  const handleClick = (e) => {
-    if (e.target.classList.contains("close")) {
-      setClickedImg(null);
-      document.body.style.overflow = "visible";
-    }
-  };
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto max-h-screen my-6 p-6 mx-auto max-w-5xl">
-          {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            {/*body*/}
-            <figure className="relative p-2 flex-auto">
-              <TransformWrapper
-                initialScale={1}
-                initialPositionX={0}
-                initialPositionY={0}
-                wheel={{disabled:true}}
-              >
-                {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-                  <div className="relative">
-                    <div className="tools">
-                      <button name="zoom-in" onClick={() => zoomIn()}>
-                        +
-                      </button>
-                      <button name="zoom-out" onClick={() => zoomOut()}>
-                        -
-                      </button>
-                      <button
-                        name="zoom-reset"
-                        onClick={() => resetTransform()}
-                      >
-                       Reset
-                      </button>
-                      <button
-                        className="close absolute right-0"
-                        name="close"
-                        onClick={handleClick}
-                      >
-                        Close
-                      </button>
-                    </div>
-                    <TransformComponent>
-                      <picture>
-                        <img loading="lazy" className="border-2 border-green border-solid" src={clickedImg.image} alt={ title ? title : `Photograph of ${locationTitle}` } />
-                      </picture>
-                    </TransformComponent>
-                  </div>
-                )}
-              </TransformWrapper>
-              {  caption ?
-                <figcaption className="my-2 text-lg leading-relaxed">
-                  { caption}
-                </figcaption>
-                : null
-              }
-            </figure>
-          </div>
-        </div>
-      </div>
-      <div className="opacity-90 fixed inset-0 z-40 bg-black"></div>
+      <ReactModal
+        isOpen={modalIsOpen}
+        overlayClassName="Overlay"
+        onRequestClose={closeModal}
+        aria={{
+          describedby: "description",
+        }}
+        style={{
+          overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.95)',
+            zIndex: 999,
+            maxHeight: "100vh",
+          },
+          content: {
+            position: 'absolute',
+            top: '40px',
+            left: '40px',
+            right: '40px',
+            bottom: '40px',
+            background: '#fff',
+            overflow: 'auto',
+            WebkitOverflowScrolling: 'touch',
+            borderRadius: '4px',
+            outline: 'none',
+            padding: '20px',
+            maxWidth: "64rem",
+            margin: "1.5rem auto",
+          }
+        }}
+      >
+        <figure>
+          <TransformWrapper
+            initialScale={1}
+            initialPositionX={0}
+            initialPositionY={0}
+            wheel={{ disabled: true }}
+          >
+            {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+              <div className="relative">
+                <div className="tools">
+                  <button name="zoom-in" onClick={() => zoomIn()}>
+                    +
+                  </button>
+                  <button name="zoom-out" onClick={() => zoomOut()}>
+                    -
+                  </button>
+                  <button name="zoom-reset" onClick={() => resetTransform()}>
+                    Reset
+                  </button>
+                  <button
+                    className="close absolute right-0"
+                    name="close"
+                    onClick={closeModal}
+                  >
+                    Close
+                  </button>
+                </div>
+                <TransformComponent>
+                  <picture>
+                    <img
+                      loading="lazy"
+                      className="border-2 border-green border-solid"
+                      src={clickedImg.image}
+                      alt={title ? title : `Photograph of ${locationTitle}`}
+                      tabIndex="0"
+                    />
+                  </picture>
+                </TransformComponent>
+              </div>
+            )}
+          </TransformWrapper>
+          {caption ? (
+            <figcaption className="my-2 text-lg leading-relaxed" id="description">
+              {caption}
+            </figcaption>
+          ) : null}
+        </figure>
+      </ReactModal>
     </>
   );
+  // return (
+  //   <>
+  //     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+  //       <div className="relative w-auto max-h-screen my-6 p-6 mx-auto max-w-5xl">
+  //         {/*content*/}
+  //         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+  //           {/*body*/}
+  //           <figure className="relative p-2 flex-auto">
+  //             <TransformWrapper
+  //               initialScale={1}
+  //               initialPositionX={0}
+  //               initialPositionY={0}
+  //               wheel={{disabled:true}}
+  //             >
+  //               {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+  //                 <div className="relative">
+  //                   <div className="tools">
+  //                     <button name="zoom-in" onClick={() => zoomIn()}>
+  //                       +
+  //                     </button>
+  //                     <button name="zoom-out" onClick={() => zoomOut()}>
+  //                       -
+  //                     </button>
+  //                     <button
+  //                       name="zoom-reset"
+  //                       onClick={() => resetTransform()}
+  //                     >
+  //                      Reset
+  //                     </button>
+  //                     <button
+  //                       className="close absolute right-0"
+  //                       name="close"
+
+  //                     >
+  //                       Close
+  //                     </button>
+  //                   </div>
+  //                   <TransformComponent>
+  //                     <picture>
+  //                       <img loading="lazy" className="border-2 border-green border-solid" src={clickedImg.image} alt={ title ? title : `Photograph of ${locationTitle}` } />
+  //                     </picture>
+  //                   </TransformComponent>
+  //                 </div>
+  //               )}
+  //             </TransformWrapper>
+  //             {  caption ?
+  //               <figcaption className="my-2 text-lg leading-relaxed">
+  //                 { caption}
+  //               </figcaption>
+  //               : null
+  //             }
+  //           </figure>
+  //         </div>
+  //       </div>
+  //     </div>
+  //     <div className="opacity-90 fixed inset-0 z-40 bg-black"></div>
+  //   </>
+  // );
 }
--- components/post-body.component.js
+++ components/post-body.component.js
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import Modal from "./modal.component";
+import ReactModal from "react-modal";
 
 export default function PostBody({ title, gallery, content }) {
   const [clickedImg, setClickedImg] = useState(null);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const handleClick = (img) => {
     setClickedImg(img);
-    console.log(img)
+    console.log(img);
     document.body.style.overflow = "hidden";
   };
 
   const handleKeyDown = (img) => {
     setClickedImg(img);
+  };
+
+  const openModal = (img) => {
+    setIsOpen(true);
+    setClickedImg(img)
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
   }
 
   return (
@@ -28,11 +39,12 @@ export default function PostBody({ title, gallery, content }) {
                 <img
                   loading="lazy"
                   src={img.image}
-                  onClick={() => handleClick(img)}
-                  alt={ img.title ? img.title : `Photograph of ${title}` }
+                  onClick={() => openModal(img)}
+                  alt={img.title ? img.title : `Photograph of ${title}`}
                   tabIndex="0"
                   onKeyDown={() => handleKeyDown(img)}
                   key={i}
+                  onKeyDown={e => e.key === 'Enter' && openModal(img)}
                 />
               </picture>
               {img.title ? (
@@ -50,6 +62,8 @@ export default function PostBody({ title, gallery, content }) {
               clickedImg={clickedImg}
               setClickedImg={setClickedImg}
               locationTitle={title}
+              modalIsOpen={modalIsOpen}
+              closeModal={closeModal}
             />
           )}
         </div>
