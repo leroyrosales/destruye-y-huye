a7e1ced443f348c5d8a5202bf844e9ef525f9de0
Leroy Rosales <leroyrosales@gmail.com>
2022-03-15 19:44:39 -0500

    Init tina setup and mapping

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/index.css'
+import Tina from '../.tina/components/TinaDynamicProvider.js'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/index.css'
+const App = ({ Component, pageProps }) => {
+  return (
+    <Tina>
+      <Component {...pageProps} />
+    </Tina>
+  )
 }
+
+export default App
+
